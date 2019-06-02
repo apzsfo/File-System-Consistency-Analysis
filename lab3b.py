281dc5c8566ed8fbccd45a031b8217e8fdf3e2e0
Andrew Zeff <andrewzeff@Andrews-MacBook-Pro.local>
2019-06-01 23:01:49 -0700

    main function kind of set up

--- lab3b.py
+++ lab3b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 import csv
-import string
+
 
 
 
@@ -9,7 +9,18 @@ import string
 
 
 def main():
+    if len(sys.argv) != 2:
+        sys.stderr.write("Requires exactly 1 argument\n")
+        exit(1)
+    try:
+        infile = open(sys.argv[1])
+    except IOError:
+        sys.stderr.write("Error opening file system image\n")
+        exit(1)
 
+
+    infile.close()
+    exit(0)
 
 
 
