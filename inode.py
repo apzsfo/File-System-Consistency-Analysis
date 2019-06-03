1250c9e354e57c2832bd3f97d7f6cf89047699e3
Andrew Zeff <apzsfo@gmail.com>
2019-06-02 22:20:02 -0700

    Update inode.py

--- inode.py
+++ inode.py
@@ -1,10 +1,13 @@
+#NAME: Ryan Nemiroff, Andrew Zeff
+#EMAIL: ryguyn@gmail.com, apzsfo@g.ucla.edu#
+#ID: 304903942, 804994987
 #!/usr/bin/env python3
 
 import sys
 import csv
 
-def inodeAllocationAudit(file, numInodes, startInode):
-    file.seek(0)
+def inodeAllocationAudit(file, numInodes, startInode): #allocating inodes
+    file.seek(0) #start at beginning
     allocatedInodes = [False]*numInodes
     freelistInodes = [False]*numInodes
     for line in file:
@@ -79,12 +82,81 @@ def main():
             startInode = int(line[7])-1
 
     #Block Consistency Audits
+    num_blocks = 0
+    num_inodes = 0
+    block_size = 0
+    inode_size = 0
+
+    direct_key = {0: "BLOCK", 1: "INDIRECT BLOCK", 2: "DOUBLE INDIRECT BLOCK", 3: "TRIPLE INDIRECT BLOCK"}
+    single_block = {"bfree": 0, "referenced": 0, "direction" : [0], "offset" : [-1], "inode_nums" : []} #each block has info
+    block_info = {}
+    #inode_dict = {}
+
+    for csv in infile:
+        parsed = csv.split(',')
+        if parsed[0] == 'SUPERBLOCK': #get sizes
+            num_blocks = int(parsed[1])
+            num_inodes = int(parsed[2])
+            block_size = int(fields[3])
+            inode_size = int(fields[4])
+        if parsed[0] == 'INODE': #capture blocks corresponding to particular inodes
+            #inode_dict[int(parsed[1])] = []
+            for num in range(12, 27):
+                if int(parsed[num]) not in block_info
+                    block_info[int(parsed[num])] = {"bfree": 0, "referenced": 1, "direction" : [0], "offset" : [num - 12], "inode_num" : [int(parsed[1])]}
+                    #inode_dict[int(parsed[1])].append(int(parsed[num]))
+                else:
+                    #inode_dict[int(parsed[1])].append(int(parsed[num]))
+                    block_info[int(parsed[num])]["referenced"] += 1
+                    block_info[int(parsed[num])]["inode_num"].append(int(parsed[1]))
+                    block_info[int(parsed[num])]["offset"].append(num - 12)
+                    block_info[int(parsed[num])]["direction"].append(0)
+                length = len(block_info[int(parsed[num])]["direction"])
+                if num == 24:
+                    block_info[int(parsed[num])]["direction"][length-1] = 1 #handling the indirect cases
+                
+                if num == 25:
+                    block_info[int(parsed[num])]["direction"][length-1] = 2
+                    block_info[int(parsed[num])]["offset"][length-1] = 12 + 256
+                if num == 26:
+                    block_info[int(parsed[num])]["direction"][length -1] = 3
+                    block_info[int(parsed[num])]["offset"][length -1] = 12 + 256 + 256*256
+
+
+    file.seek(0) #start at beginning of file
+    for csv in infile:
+        parsed = csv.split(',')
+        if parsed[0] == 'GROUP': #find information for the first valid block
+            first_valid = int(parsed[8]) + inode_size * num_inodes / block_size
+        if parsed[0] == 'BFREE': #check free list
+            if int(parsed[1]) in block_info
+                block_info[int(parsed[1])]["bfree"] = 1
+            else:
+                block_info[int(parsed[1])] = {"bfree": 1, "referenced": 0, "direction" : [0], "offset" : [-1], "inode_nums" : []}
+
+    for num in block_info: #loop through all blocks
+        length = len(block_info[num]["direction"])
+        for i in range (0, length): #duplicate check
+            allocated_conditional = 0
+            reserved = 0
+            invalid = 0
+            if (num < 0 or num > num_blocks - 1) and block_info[num]["offset"][i] != -1:
+                sys.stdout.write("INVALID " + direct_key[block_info[num]["direction"][i]] + " " + num + " IN INODE " + block_info[num]["inode_num"][i] + " AT OFFSET " + block_info[num]["offset"][i] + "\n")
+                invalid = 1
+            elif num > 0 and num < first_valid and block_info[num]["offset"][i] != -1:
+                sys.stdout.write("RESERVED " + direct_key[block_info[num]["direction"][i]] + " " + num + " IN INODE " + block_info[num]["inode_num"][i] + " AT OFFSET " + block_info[num]["offset"][i] + "\n")
+                reserved = 1
+            if block_info[num]["referenced"] == 0 and block_info[num]["bfree"] == 0 and reserved = 0 and invalid = 0:
+                sys.stdout.write("UNREFERENCED BLOCK " + num)
+            if block_info[num]["bfree"] == 1 and block_info["referenced"] > 0 and reserved == 0 and invalid == 0 and allocated_conditional == 0:
+                sys.stdout.write("ALLOCATED BLOCK " + num + " ON FREELIST" + "\n")
+                allocated_conditional = 1
+            if block_info["referenced"] > 1 and reserved = 0 and invalid = 0:
+                sys.stdout.write("DUPLICATE " + direct_key[block_info[num]["direction"][i]] + " " + num + "\n")
 
     #I-node Allocation Audits
     inodeAllocationAudit(infile, numInodes, startInode)
     directoryConsistencyAudit(infile, numInodes)
-
-    #Directory Consistency Audits
 
     infile.close()
 
